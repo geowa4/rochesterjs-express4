bdd1374e15a113bf2197e34f9c08560277d9b074
George Adams IV <g.w.adams.iv@gmail.com>
2014-09-07 22:03:01 -0400

    add query params

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ express = require('express')
 app = express()
 
 app.get('/', function (req, res){
-  res.send('Hello World')
+  res.send('Hello ' + (req.query['id'] || 'World'))
 })
 
 server = app.listen(3000, function () {
